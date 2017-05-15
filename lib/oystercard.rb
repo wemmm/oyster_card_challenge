8ee7d70ffa4f4dd04d4ec81c4656e3b163df78b9
wemmm <jenny.wem@gmail.com>
2017-05-15 15:39:52 +0100

    Added failing test for balance method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,7 @@
-class OysterCard
+class Oystercard
+
+  def balance
+    
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require 'oystercard'
 
-describe OysterCard do
+describe Oystercard do
+
+  subject(:card) { Oystercard.new }
+
+  describe '#balance' do
+    it 'returns the balance' do
+      expect(card.balance).to eq 0
+    end
+  end
+
 end
