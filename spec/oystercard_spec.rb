d300ddc374b78210d0dd7f1052778f4f4311281b
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 16:12:05 +0100

    Refactor top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,8 @@ describe Oystercard do
 
   subject(:card) { Oystercard.new }
 
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
   describe '#balance' do
     it 'returns the balance' do
       expect(card.balance).to eq 0
@@ -11,7 +13,6 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
     it 'increases the balance of the card by the specified amount' do
       card.top_up(100)
       expect(card.balance).to eq 100
